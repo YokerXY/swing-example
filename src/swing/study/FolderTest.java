82c472dd43d94a736d899ccfd7732baba97e97a7
ASUS <2414704205@qq.com>
2019-04-02 01:06:51 +0800

    练习

--- src/swing/study/FolderTest.java
+++ src/swing/study/FolderTest.java
@@ -7,14 +7,13 @@ import java.io.File;
  * 2019.4.1
  */
 public class FolderTest {
-    public static void main(String[] args) {
-        //遍历路径
+    public static void main(String[] args) { //遍历路径
         String path = "D:\\java1";
         //获取file对象
         File file = new File(path);
         //遍历path下文件
         File[] fs = file.listFiles();
-        for (File f : fs) {
+        for (File f :fs){
             String srcFileName = f.getName();
             int position = srcFileName.indexOf(".");
             String suffixName = srcFileName.substring(position);
